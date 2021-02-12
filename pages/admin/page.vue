4c1086094c1465a1385b520800bd7717779c86f2
shakechi <sctefh.dev@gmail.com>
2021-02-12 13:05:25 +0900

    create page

---
+++ pages/admin/page.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>(JP)管理者のみが観覧できるページ</h1>
+    <h1>(EN)Pages that can only be viewed by administrators</h1>
+  </div>
+</template>
---
+++ pages/secret/page.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>(JP)認証が完了しているユーザーは見れる</h1>
+    <h1>(EN)Users who have been authenticated can see</h1>
+  </div>
+</template>
